e3b98fb8e45ee96245f72a224f0cd766efa66746
Addi-Blocher <85126693+Addi-Blocher@users.noreply.github.com>
2022-10-12 19:27:54 -0600

    ica8

---
+++ js/ica8.js
@@ -0,0 +1,9 @@
+let body = document.querySelector
+body.addEventListener('click', giveAlert());
+
+function giveAlert() {
+    alert('this is an alert');
+}
+
+body.addEventListener('click', onclick(event))
+document.body.style.backgroundColor = 'salmon';
