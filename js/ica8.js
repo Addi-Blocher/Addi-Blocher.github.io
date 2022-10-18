b8bd98abc85c4ed497c17d770250541e8a7d8760
Addi-Blocher <85126693+Addi-Blocher@users.noreply.github.com>
2022-10-18 10:05:28 -0600

    yeet

--- js/ica8.js
+++ js/ica8.js
@@ -7,3 +7,23 @@ function giveAlert() {
 
 body.addEventListener('click', onclick(event))
 document.body.style.backgroundColor = 'salmon';
+
+
+let postersSold = false;
+let moneyMade;
+
+if (postersSold === true) {
+    moneyMade = 10000000000000;
+  } else {
+    moneyMade = -2500;
+  }
+
+  function lNames(names) {
+    return names.startsWith('M');
+  }
+  
+  const names = ['Macy', 'Amy', 'Mona', 'Vera', 'Mcgregor', 'Jeremy'];
+  
+  const filtered = names.filter(lNames);
+  
+  console.log(filtered);
