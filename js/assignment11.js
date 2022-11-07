a27a9a88c590b0002c9939eb90410480fc3c5a94
Addi-Blocher <85126693+Addi-Blocher@users.noreply.github.com>
2022-11-06 23:09:49 -0700

    sad

--- js/assignment11.js
+++ js/assignment11.js
@@ -3,18 +3,25 @@
 const quoteButton = document.querySelector('.new-quote');
 quoteButton.addEventListener('click', getQuote);
 
-const endpoint = "https://api.https://api.namefake.com/.io/?name=cory";
+const endpoint = "https://api.breakingbadquotes.xyz/v1/quotes";
 
-async function getQuote() {
-    //console.log('test click worked');
-    let text =  await fetch(endpoint);
-    let response = await text.text();
+// async function getQuote() {
+//     //console.log('test click worked');
+//     let text =  await fetch(endpoint);
+//     let response = await text.text();
     
-    let json_response = JSON.parse(response);
-    //console.log (json_response);
-    console.log(json_response['message']);
+//     let json_response = JSON.parse(response);
+//     //console.log (json_response);
+//     console.log(json_response['message']);
 
-    displayQuote (json_response['message']);
+//     displayQuote (json_response['message']);
+// }
+
+function getQuote()
+{
+    let text = fetch(endpoint)
+    .then(response=>response.text())
+    .then(text=>displayQuote(text))
 }
 
 
@@ -26,8 +33,8 @@ function displayQuote(x) {
     //const textMessage = document.createTextNode(x);
 
     //quoteBox.appendChild(textMessage);
-
-    document.getElementById('js-quote-text').textContent = x;
+    let text = JSON.parse(x);
+    document.getElementById('js-quote-text').textContent = text['quote'];
 }
 
 
