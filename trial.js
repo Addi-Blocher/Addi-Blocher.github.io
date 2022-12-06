57432e4f0eb8be1abc54a285bf16a0f65e3e6fdf
Addi-Blocher <85126693+Addi-Blocher@users.noreply.github.com>
2022-12-06 14:16:50 -0700

    update

--- trial.js
+++ trial.js
@@ -11,15 +11,6 @@ img.onclick = function(){
   captionText.innerHTML = this.alt;
 }
 
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img02");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
